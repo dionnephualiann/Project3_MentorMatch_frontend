92018f1f24f4eb38566965f7ec8c2f5708462e83
Dionne Phua <li.ann86@gmail.com>
2017-07-08 14:43:02 +0800

    Added react-router-dom. NavBar using the React-bootstrap. Added images. Setting up the events page with eventPost, eventPage, eventBody, eventList.

---
+++ src/components/events/eventPage.js
@@ -0,0 +1,13 @@
+// event page contains all Lists, Body and Post
+import React, {PropTypes} from 'react';
+// import EventPost from "./eventPost";
+// import EventList from "./eventlist";
+
+
+export const EventPage = (props) => (
+<div>
+hello
+  {/*<EventPost />
+  // <EventList />*/}
+</div>
+);
