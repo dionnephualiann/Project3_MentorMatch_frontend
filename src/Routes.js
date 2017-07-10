c6a0620a7c9bd7b539fe69f6c30bab18a8ec196c
Bulner xavier wittebrown <b.x.w@live.com>
2017-07-10 14:10:18 +0800

    Setup login and signup page. able to console log all Signup input field and Login input fields. have not setup routes from login page to signup page

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,11 @@
 import React, {PropTypes} from 'react';
+<<<<<<< HEAD
+=======
+import EventPage from './components/events/eventPage';
+import SignUp from './components/SignUp/SignUp';
+import Login from './components/Login/Login';
+import LandingPage from './components/LandingPage/LandingPage';
+>>>>>>> Setup login and signup page. able to console log all Signup input field and Login input fields. have not setup routes from login page to signup page
 import {Switch, Route} from 'react-router-dom';
 import EventPage from './components/events/eventPage';
 import ProfilePage from './components/profile/profile';
@@ -12,6 +19,9 @@ const Main = () => (
   <Switch>
     <Route path = '/events' component = {EventPage}></Route>
     <Route path = '/profile' component = {ProfilePage}></Route>
+    <Route path = '/Login' component = {Login}></Route>
+    <Route path = '/SignUp' component = {SignUp}></Route>
+    <Route path = '/LandingPage' component = {LandingPage}></Route>
   </Switch>
 
 
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,119 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Button, FormGroup, InputGroup, ControlLabel, FormControl, Glyphicon, Row, Col, Form, Checkbox } from 'react-bootstrap';
+import './SignUp.css'
+
+
+export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      password: '',
+      title: '',
+      describe: '',
+      skills: ''
+    }
+  }
+
+  onChange = (e) => {
+    let state = this.state
+    let field = e.target.name;
+    let value = e.target.value;
+    state[field] = value
+    console.log(field, value)
+    this.setState(state)
+    console.log('state', this.state)
+  }
+
+  onClick = (e) => {
+
+    console.log(this.state.email,
+                this.state.password,
+                this.state.title,
+                this.state.describe,
+                this.state.skills)
+  }
+
+
+  render() {
+    return (<div>
+
+    <Row className="show-grid">
+        <Col md={6} mdPush={6}>
+        <FormGroup bsSize="large">
+
+        <div className="SignUpField">
+          <p>Skills:</p>
+            <FormControl
+            id="formControlsText"
+            type="skills"
+            name="skills"
+            placeholder="List Your Skills"
+            onChange={this.onChange}/>
+        </div>
+        </FormGroup>
+
+
+        <button type="button"
+        class="btn btn-primary"
+        onClick={this.onClick}>
+        Sign Up</button>
+        </Col>
+
+        <Col md={6} mdPull={6}>
+        <FormGroup bsSize="large">
+
+          <div className="SignUpField">
+            <p>Name:</p>
+              <FormControl
+              id="formControlsText"
+              type="name"
+              name="name"
+              placeholder="Name"
+              onChange={this.onChange}
+              />
+          </div>
+
+          <div className="SignUpField">
+            <p>Password:</p>
+              <FormControl
+              id="formControlsText"
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={this.onChange}
+              />
+          </div>
+
+          <div className="SignUpField">
+            <p>Job Title / Business Title:</p>
+              <FormControl
+              id="formControlsText"
+              type="title"
+              name="title"
+              placeholder="Job Title / Business Title"
+              onChange={this.onChange}
+              />
+          </div>
+
+          <div className="SignUpField">
+            <p>Describe Yourself:</p>
+              <FormControl
+                id="formControlsText"
+                type="describe"
+                name="describe"
+                placeholder="Describe Yourself"
+                onChange={this.onChange}/>
+          </div>
+
+        </FormGroup>
+
+        </Col>
+      </Row>
+
+
+</div>);
+  }
+}
